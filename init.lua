65277e84c0e2dd0b6e3052891f6bfdcbed26b339
Park Junha <bd08001@gmail.com>
2026-05-01 21:47:45 +0800

    setup which-key dependencies

--- init.lua
+++ init.lua
@@ -4,8 +4,15 @@ vim.g.mapleader = " "
 
 vim.pack.add({'https://github.com/folke/snacks.nvim'})
 Snacks = require('snacks')
+
+--which-key
 vim.pack.add({'https://github.com/folke/which-key.nvim'})
 WhichKey = require('which-key')
+-- which-key dependencies
+vim.pack.add({'https://github.com/nvim-mini/mini.nvim'})
+require('mini.icons').setup()
+vim.pack.add({'https://github.com/nvim-tree/nvim-web-devicons'})
+require('nvim-web-devicons').setup()
 
 Snacks.setup(
 	{
@@ -135,3 +142,25 @@ vim.keymap.set("n",
         require("which-key").show({ global = false })
       end,
       {desc = "Buffer Local Keymaps (which-key)"})
+
+-- WSL clipboard setup
+vim.g.clipboard = {
+  name = "WslClipboard",
+  copy = {
+    ["+"] = "clip.exe",
+    ["*"] = "clip.exe",
+  },
+  paste = {
+    ["+"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ["*"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  },
+  cache_enabled = 0,
+}
+vim.opt.clipboard:append({ "unnamedplus" })
+
+
+
+vim.pack.add({'https://github.com/nvim-mini/mini.nvim'})
+MiniFiles = require('mini.files')
+MiniFiles.setup()
+vim.keymap.set("n", "<leader>fm", function() MiniFiles.open() end, {desc = "MiniFiles Open"})
