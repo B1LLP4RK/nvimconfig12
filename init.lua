da6d749d87dbff057f88f0f473912dc355bf4041
Park Junha <bd08001@gmail.com>
2026-05-01 20:31:29 +0800

    setup WSL clipboard

--- init.lua
+++ init.lua
@@ -135,3 +135,18 @@ vim.keymap.set("n",
         require("which-key").show({ global = false })
       end,
       {desc = "Buffer Local Keymaps (which-key)"})
+
+-- WSL clipboard setup
+vim.g.clipboard = {
+  name = "WslClipboard",
+  copy = {
+    ["+"] = "clip.exe",
+    ["*"] = "clip.exe",
+  },
+  paste = {
+    ["+"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ["*"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  },
+  cache_enabled = 0,
+}
+vim.opt.clipboard:append({ "unnamedplus" })
