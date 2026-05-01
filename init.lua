09430208a0eaed91ac804cb255de6cb3eb5d43ed
Park Junha <bd08001@gmail.com>
2026-05-01 21:50:23 +0800

    remove wsl specific clipboard setting

--- init.lua
+++ init.lua
@@ -143,22 +143,6 @@ vim.keymap.set("n",
       end,
       {desc = "Buffer Local Keymaps (which-key)"})
 
--- WSL clipboard setup
-vim.g.clipboard = {
-  name = "WslClipboard",
-  copy = {
-    ["+"] = "clip.exe",
-    ["*"] = "clip.exe",
-  },
-  paste = {
-    ["+"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    ["*"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-  },
-  cache_enabled = 0,
-}
-vim.opt.clipboard:append({ "unnamedplus" })
-
-
 
 vim.pack.add({'https://github.com/nvim-mini/mini.nvim'})
 MiniFiles = require('mini.files')
