6c21bcde2342d7e95d405f3529358793a5581e3e
Park Junha <bd08001@gmail.com>
2026-05-05 02:34:00 +0800

    setup minifiles keymap

--- plugin/mini.lua
+++ plugin/mini.lua
@@ -1,4 +1,6 @@
 vim.pack.add({'https://github.com/nvim-mini/mini.nvim'})
 MiniFiles = require('mini.files')
 MiniFiles.setup()
-vim.keymap.set("n", "<leader>fm", function() MiniFiles.open() end, {desc = "MiniFiles Open"})
+-- Open directory of current file (in last used state) focused on the file
+vim.keymap.set("n", "<leader>fm", function() MiniFiles.open(vim.api.nvim_buf_get_name(0)) end, {desc = "MiniFiles Open"})
+vim.keymap.set("n", "<leader>fM", function() MiniFiles.open() end, {desc = "MiniFiles Open"})
