5ca78be15543d9606dc8688e14004c015a346c58
Park Junha <bd08001@gmail.com>
2026-05-07 23:47:33 +0800

    add shortcut

--- plugin/mini.lua
+++ plugin/mini.lua
@@ -4,3 +4,4 @@ MiniFiles.setup()
 -- Open directory of current file (in last used state) focused on the file
 vim.keymap.set("n", "<leader>fm", function() MiniFiles.open(vim.api.nvim_buf_get_name(0)) end, {desc = "MiniFiles Open"})
 vim.keymap.set("n", "<leader>fM", function() MiniFiles.open() end, {desc = "MiniFiles Open"})
+vim.keymap.set("n", "<leader>fd", function() MiniFiles.open(vim.fn.stdpath("data")) end, {desc = "data dir Open"})
