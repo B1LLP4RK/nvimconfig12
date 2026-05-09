ef3002a5b99c881d9167190465e4ac77ddf66d51
Park Junha <bd08001@gmail.com>
2026-05-09 09:48:06 +0800

    setup autopairing for parenthesis, bracket, etc

--- plugin/mini.lua
+++ plugin/mini.lua
@@ -5,3 +5,17 @@ MiniFiles.setup()
 vim.keymap.set("n", "<leader>fm", function() MiniFiles.open(vim.api.nvim_buf_get_name(0)) end, {desc = "MiniFiles Open"})
 vim.keymap.set("n", "<leader>fM", function() MiniFiles.open() end, {desc = "MiniFiles Open"})
 vim.keymap.set("n", "<leader>fd", function() MiniFiles.open(vim.fn.stdpath("data")) end, {desc = "data dir Open"})
+
+MiniPairs = require('mini.pairs')
+MiniPairs.setup(
+	{  modes = { insert = true, command = true, terminal = false },
+	-- skip autopair when next character is one of these
+	skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+	-- skip autopair when the cursor is inside these treesitter nodes
+	skip_ts = { "string" },
+	-- skip autopair when next character is closing pair
+	-- and there are more closing pairs than opening pairs
+	skip_unbalanced = true,
+	-- better deal with markdown code blocks
+	markdown = true,}
+)
