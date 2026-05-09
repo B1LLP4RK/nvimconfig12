621e35cd88ec1550b0a65ff79d71d76494263d59
Park Junha <bd08001@gmail.com>
2026-05-09 09:47:46 +0800

    fix python lsp issue in WSL Ubuntu

--- plugin/wsl.lua
+++ plugin/wsl.lua
@@ -12,3 +12,18 @@ vim.g.clipboard = {
   cache_enabled = 0,
 }
 vim.opt.clipboard:append({ "unnamedplus" })
+
+
+-- python lsp issue fix
+
+vim.pack.add({ 'https://github.com/neovim/nvim-lspconfig' })
+vim.pack.add({"https://github.com/mason-org/mason.nvim"})
+require("mason").setup()
+-- this package automatically calls `vim.lsp.enable` to correct filetypes
+vim.pack.add({"https://github.com/mason-org/mason-lspconfig.nvim"})
+require("mason-lspconfig").setup()
+local mason_path = vim.fn.stdpath("data") .. "/mason"
+local pyright_langserver = mason_path .. "/packages/pyright/node_modules/pyright/langserver.index.js"
+vim.lsp.config("pyright", {
+  cmd = { "node", pyright_langserver, "--stdio" },
+})
